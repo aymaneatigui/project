e49c9019e397cb5e97f74554895aff8cbc2c6e18
aymane <aymane.atigui.0202@gmail.com>
2023-07-20 16:42:57 +0100

    create model user

--- appAPI/models.py
+++ appAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    pwd = models.CharField(max_length=128)
+
+    def __str__(self) -> str:
+        return self.username
