18c94db83eb6e8c314c6ed1db774a5d9d4214965
aymane <aymane.atigui.0202@gmail.com>
2023-07-26 18:20:17 +0100

    Search and Ordering

--- appAPI/views.py
+++ appAPI/views.py
@@ -3,20 +3,18 @@ from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelM
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from rest_framework import status
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from appAPI.models import MyUser
 from appAPI.Serializer import UserSerializer
-from  django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter
-
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class UsersList(GenericAPIView, ListModelMixin, CreateModelMixin):
     queryset = MyUser.objects.all()
     serializer_class = UserSerializer
-    filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]  
-    search_fields = ['^username', 'email', '^fname', '^lname']  
+    filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter, OrderingFilter]  
+    search_fields = ['^username','^fname', '^lname']  
     filterset_fields =['username','fname','lname']
+    ordering_fields = ['username']
 
     def get(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
@@ -25,6 +23,7 @@ class UsersList(GenericAPIView, ListModelMixin, CreateModelMixin):
             'Users Number': queryset.count(),
             'Users': serializer_data.data
         }, status = status.HTTP_200_OK)
+    
     def post(self, request, *args, **kwargs):
         serializer_data = UserSerializer(data = request.data)
         if serializer_data.is_valid():
