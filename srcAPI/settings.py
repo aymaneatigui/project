a15e269a66996ce6e0ea5914c2abfbe15ecb6cec
aymane <aymane.atigui.0202@gmail.com>
2023-07-26 19:06:30 +0100

    rest framework sittings

--- srcAPI/settings.py
+++ srcAPI/settings.py
@@ -120,3 +120,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'appAPI.MyUser'
 AUTH_PROFILE_MODULE = 'appAPI.MyUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+
+}
